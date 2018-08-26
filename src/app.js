c95c2cee8116724a75d5fd29e457f0d183686537
mcy <201102430@cs-cnu.org>
2018-08-27 01:19:42 +0900

    feat(state): 상태정의

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 import * as _ from 'partial-js';
 import { BehaviorSubject } from 'rxjs';
+
+document.addEventListener("DOMContentLoaded", e => {
+  // 상태정의
+  const hash = new BehaviorSubject("");
+  const things = new BehaviorSubject([]);
+});
