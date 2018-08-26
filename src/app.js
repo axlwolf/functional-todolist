cf0174ec53cae3f64980e3b67ff9583ecd029db6
mcy <201102430@cs-cnu.org>
2018-08-27 01:23:09 +0900

    feat(hash_state): 해쉬 변경시 스테이트 업데이트 이벤트 등록

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import * as _ from 'partial-js';
 import { BehaviorSubject } from 'rxjs';
 
-document.addEventListener("DOMContentLoaded", e => {
+const $ = document.addEventListener;
+
+$("DOMContentLoaded", e => {
+  console.log("DOMLoaded");
   // 상태정의
   const hash = new BehaviorSubject("");
   const things = new BehaviorSubject([]);
+
+  $("hashchange", e => {
+    const hashPath = location.hash.replace("#/", "");
+
+    hash.next(hashPath);
+  });
 });
